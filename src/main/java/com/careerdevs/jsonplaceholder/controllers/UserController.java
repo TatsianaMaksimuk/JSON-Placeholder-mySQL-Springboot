df66b7139d3c9d98cad6a2ece6e804b336a81504
Tatsiana Maksimuk <okeanoka@gmail.com>
2022-08-11 22:14:53 -0700

    "routes added"

--- src/main/java/com/careerdevs/jsonplaceholder/controllers/UserController.java
+++ src/main/java/com/careerdevs/jsonplaceholder/controllers/UserController.java
@@ -1,39 +1,80 @@
 package com.careerdevs.jsonplaceholder.controllers;
 
+import com.careerdevs.jsonplaceholder.models.UserModel;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import javax.xml.transform.Source;
+
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 public class UserController {
-    private final String jsonPlaceholderEndpoint = "https://jsonplaceholder.typicode.com";
+    private final String jsonPlaceholderEndpoint = "https://jsonplaceholder.typicode.com/users";
 
     @RequestMapping("/")
-    public String route(){
+    public String route() {
         return "Hi";
     }
 
-    @GetMapping("/users")
-    public Object usersHandler (RestTemplate restTemplate){
-        return restTemplate.getForObject(jsonPlaceholderEndpoint +"/users", Object.class);
+    @GetMapping("/all")
+    public ResponseEntity<?> getUsers(RestTemplate restTemplate) {
+        try {
+            UserModel[] response = restTemplate.getForObject(jsonPlaceholderEndpoint, UserModel[].class);
+
+
+            for (int i = 0; i < response.length; i++) {
+                UserModel user = response[i];
+                System.out.println(user.getName());
+
+            }
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            System.out.println(e.getClass());
+            System.out.println(e.getMessage());
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<?> getUserById(RestTemplate restTemplate, @PathVariable String id) {
+        try {
+            Integer.parseInt(id);
+            System.out.println("Getting user with id: " + id);
+            String url = jsonPlaceholderEndpoint + "/" + id;
+            UserModel response = restTemplate.getForObject(url, UserModel.class);
+            return ResponseEntity.ok(response);
+        } catch (NumberFormatException e) {
+            return ResponseEntity.status(400).body("Invalid id: " + id);
+        }catch (HttpClientErrorException.NotFound e) {
+            return ResponseEntity.status(404).body("User Not Found With ID: " + id);
+        } catch (Exception e) {
+            System.out.println(e.getClass());
+            System.out.println(e.getMessage());
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+
     }
 
     @GetMapping("/posts")
-    public Object postsHandler (RestTemplate restTemplate){
-        return restTemplate.getForObject(jsonPlaceholderEndpoint +"/posts", Object.class);
+    public Object postsHandler(RestTemplate restTemplate) {
+        return restTemplate.getForObject(jsonPlaceholderEndpoint + "/posts", Object.class);
     }
 
     @GetMapping("/photos")
-    public Object photoHandler(RestTemplate restTemplate){
-        return restTemplate.getForObject(jsonPlaceholderEndpoint +"/photos", Object.class);
+    public Object photoHandler(RestTemplate restTemplate) {
+        return restTemplate.getForObject(jsonPlaceholderEndpoint + "/photos", Object.class);
 
     }
 
     @GetMapping("/todos")
-    public Object toDoHandler(RestTemplate restTemplate){
-        return restTemplate.getForObject(jsonPlaceholderEndpoint +"/todos", Object.class);
+    public Object toDoHandler(RestTemplate restTemplate) {
+        return restTemplate.getForObject(jsonPlaceholderEndpoint + "/todos", Object.class);
     }
 
 }
